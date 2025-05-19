fa5146ebf1b3457dc7549220f62a2746c5fe4294
Heder Gabriel <hedergabrielsalessousa@gmail.com>
2025-05-19 12:05:36 -0300

    delete archive

--- Archive/main.php
+++
@@ -1,8 +0,0 @@
-    <main class="produto-detalhe">
-        <img src="<?= htmlspecialchars($produto['imagem']) ?>" alt="<?= htmlspecialchars($produto['nome']) ?>">
-        <h1><?= htmlspecialchars($produto['nome']) ?></h1>
-        <p><?= htmlspecialchars($descricaoCompleta) ?></p>
-        <strong>R$ <?= number_format($produto['preco'], 2, ',', '.') ?></strong>
-        <br>
-        <button>Adicionar ao carrinho</button>
-    </main>
